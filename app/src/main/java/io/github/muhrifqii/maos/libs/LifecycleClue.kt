59f89401ecac9a0346aa11a6fbe70df1d2b7413a
muhrifqii <muh_rif@live.com>
2017-01-23 15:36:56 +0700

    prepare abstraction

---
+++ app/src/main/java/io/github/muhrifqii/maos/libs/LifecycleClue.kt
@@ -0,0 +1,9 @@
+package io.github.muhrifqii.maos.libs
+
+/**
+ * Created on   : 23/01/17
+ * Author       : muhrifqii
+ * Name         : Muhammad Rifqi Fatchurrahman Putra Danar
+ * Github       : https://github.com/muhrifqii
+ * LinkedIn     : https://linkedin.com/in/muhrifqii
+ */
---
+++ app/src/main/java/io/github/muhrifqii/maos/ui/data/MyActivityResult.kt
@@ -0,0 +1,11 @@
+package io.github.muhrifqii.maos.ui.data
+
+/**
+ * Created on   : 23/01/17
+ * Author       : muhrifqii
+ * Name         : Muhammad Rifqi Fatchurrahman Putra Danar
+ * Github       : https://github.com/muhrifqii
+ * LinkedIn     : https://linkedin.com/in/muhrifqii
+ */
+class MyActivityResult {
+}
